39b9800c5ae25a21fb90fc3d3ed8b3248a73b125
mylogi <mp.gplay@gmail.com>
2021-12-18 19:09:37 +0200

    Add a task_3.

---
+++ task_3.py
@@ -0,0 +1,29 @@
+# Write a program, which has two print statements to print
+# the following text (capital letters “O” and “H”, made from “#” symbols):
+# #########
+# #       #
+# #       #
+# #       #
+# #########
+#
+# #       #
+# #       #
+# #########
+# #       #
+# #       #
+
+# My try
+
+# O:
+print('#' * 9, '\n'
+               "#"'\t''\t''#'"\n"
+               '#''\t''\t''#'"\n"
+               '#''\t''\t''#'"\n"
+               '#''#''#''#'"#"'#''#''#''#', '\n')
+
+# H:
+print('#''\t''\t''#'"\n"
+      '#''\t''\t''#'"\n"
+      '#''#''#''#'"#"'#''#''#''#''\n'
+      '#''\t''\t''#'"\n"
+      '#''\t''\t''#''\n')
