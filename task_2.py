13fead46f18782c9de612deabc5bdda3b4f84fa6
mylogi <mp.gplay@gmail.com>
2021-12-18 18:39:05 +0200

    Task_2 is ready to check.

--- task_2.py
+++ task_2.py
@@ -1,3 +1,40 @@
-print('Play with git :)')
+# Create a python program named “task2”,
+# and use the built-in function `print` in it several times.
+# Try to pass “sep”, “end” params and pass several parameters separated by commas.
+# Also, provide a comment text above each print statement,
+# mentioned above, with the expected output after execution of the particular print statement.
 
-print('Play with git in merge lesson')
+# Use `print` in different ways
+
+print('Hello, world!')
+
+print('Hello world')
+
+what = "Hello"
+who = "world"
+print(f'{what} {who}')
+
+print("""Hello, Bob!""")
+
+# Try to pass "sep"
+
+# For readability
+print('\n')
+
+print(1, 2, 3, sep='|')
+
+print(1, 2, 3, sep='\n')
+
+print('Hello!', 'Hi!', 'Glad to see you!', sep='\t')
+
+# Try to pass "end"
+# For readability
+print('\n')
+
+print(1, 2, 3, end='\n' + '-' * 10)
+
+print('\n')
+
+print(1, 2, 3, end='\t' + "-" + '\t'"Thease are numbers")
+
+# The end
