bd12d833c7e4d60ca514571502c339fcc47aa58b
mylogi <mp.gplay@gmail.com>
2021-12-19 19:55:56 +0200

    🔥 Add task_1 to the repository

---
+++ task_1.py
@@ -0,0 +1,16 @@
+"""A simple function.
+Create a simple function called favorite_movie, which takes a string containing the name of your favorite movie.
+The function should then print “My favorite movie is named {name}”.
+"""
+
+
+# My func:
+
+def favorite_movie(name):
+    print('My favorite movie is named', {name})
+
+
+if __name__ == '__main__':
+    favorite_movie('Movie43')
+    favorite_movie('My lovely nanny')
+    favorite_movie('Frankenstein')
