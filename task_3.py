2bb2bb18ad42e2daefab17709fa4137e8021bcfb
mylogi <mp.gplay@gmail.com>
2021-12-20 11:48:31 +0200

    🌅 Add the finished task_3.
    Simple calculator.

--- task_3.py
+++ task_3.py
@@ -10,4 +10,29 @@ parameter. Then return the sum or product of all the numbers in the arbitrary pa
 
 
 # The Simple calculator:
-def make_operation():
+def make_operation(*args):
+    if str(args[0]) == '+':
+        return_value = 0
+        for item in args[1:]:
+            return_value += item
+
+    elif str(args[0]) == '-':
+        return_value = args[1]
+        for item in args[2:]:
+            return_value -= item
+
+    elif str(args[0]) == '*':
+        return_value = 1
+        for item in args[1:]:
+            return_value *= item
+
+    else:
+        return_value = 'WRONG ARITHMETIC OPERATOR. TRY: "+", "-", "*"'
+
+    print(return_value)
+
+
+if __name__ == '__main__':
+    make_operation('+', 7, 7, 2)
+    make_operation('-', 5, 5, -10, -20)
+    make_operation('*', 7, 6)
