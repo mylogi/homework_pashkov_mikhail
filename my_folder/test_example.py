dc5536179a92fe050dc127e043328a5a7b578ea4
mylogi <mp.gplay@gmail.com>
2021-12-20 16:17:16 +0200

    🌮 Add test_example.
    And move this file in my_folder.

---
+++ my_folder/test_example.py
@@ -0,0 +1,22 @@
+import unittest
+
+import function_example
+
+
+class NameTestCase(unittest.TestCase):
+    def test_first_and_last_names(self):
+        full = function_example.full_name('carl', 'johnson')
+        full2 = function_example.full_name('cARL', 'jOHNSON')
+        self.assertEqual(full, 'Carl Johnson')
+        self.assertEqual(full2, 'Carl Johnson')
+
+
+class SquaresTestCase(unittest.TestCase):
+
+    def test_squares(self):
+        l = [1, 2, 3, 4]
+        l2 = function_example.squares(l)
+        self.assertEqual(l2, [1, 4, 9, 16])
+
+
+unittest.main()
