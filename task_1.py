075c292c8654b2309994c1fc9d0fe8311464852b
mylogi <mp.gplay@gmail.com>
2021-12-21 16:09:28 +0200

    🔥 Add the finished task_1.

--- task_1.py
+++ task_1.py
@@ -1,1 +1,21 @@
+""" Make a program that has your name and the current day of the week stored as separate variables and then prints
+a message like this:
 
+     “Good day <name>! <day> is a perfect day to learn some python.”
+
+Note that <name> and <day> are predefined variables in source code.
+An additional bonus will be to use different string formatting methods for constructing result string.
+"""
+
+# Program:
+# Variables:
+name = "michael"
+day = "tuesday"
+
+
+def good_day():
+    return print(f'"Good day {name.title()}! {day.title()} is a perfect day to learn some python."')
+
+
+if __name__ == '__main__':
+    good_day()
