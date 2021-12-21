299c620d0aa1faccb086c79a7e99b9fb1f979c1b
mylogi <mp.gplay@gmail.com>
2021-12-21 16:43:26 +0200

    🔥 Add the finished task_2.

--- task_2.py
+++ task_2.py
@@ -1,1 +1,12 @@
+"""Manipulate strings.
 
+Save your first and last name as separate variables, then use string concatenation to add them together with a white
+space in between and print a greeting.
+"""
+
+# Task_2:
+
+first_name = 'Michael'
+last_name = 'Pashkov'
+
+print('Hello slow', first_name + " " + last_name + "!", 'Look already snow!')
