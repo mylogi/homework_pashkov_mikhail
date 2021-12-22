29f4a587f7f673083b2af9af157c5efc13563670
mylogi <mp.gplay@gmail.com>
2021-12-22 11:30:26 +0200

    🔥 Added the task_2 in lesson_6.

---
+++ task_2.py
@@ -0,0 +1,13 @@
+"""Manipulate strings.
+
+Save your first and last name as separate variables, then use string concatenation to add them together with a white
+space in between and print a greeting.
+"""
+
+# Doing:
+
+first_name = 'michael'
+last_name = 'pashkov'
+string = first_name.title() + ' ' + last_name.title()
+
+print(f'Hello, {string}! Have a nice day!')
