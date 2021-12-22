9048dd4728c1eca27b18e9e2186236ae7798c8c2
mylogi <mp.gplay@gmail.com>
2021-12-22 13:12:10 +0200

    🔥 Added the task_3 in lesson_06.

---
+++ task_3.py
@@ -0,0 +1,65 @@
+"""The name check.
+
+Write a program that has a variable with your name stored (in lowercase) and then asks for your name as input. The
+program should check if your input is equal to the stored name even if the given name has another case, e.g.,
+if your input is “Anton” and the stored name is “anton”, it should return True.
+"""
+
+
+# Program:
+
+# def name_check():
+#     name = 'mikhail'
+#     user_name = input('Enter your name: ')
+#
+#     if name == user_name.lower():
+#         return True
+#     else:
+#         return False
+#
+#
+# if __name__ == '__main__':
+#     print(name_check())
+
+# Program_2:
+
+# def name_check():
+#     name = 'mikhail'
+#     user_name = input('Enter your name: ')
+#
+#     return True if name == user_name.lower() else False
+#
+#
+# if __name__ == '__main__':
+#     print(name_check())
+
+# Program_3:
+
+# def name_check():
+#     name = 'mikhail'
+#     user_name = input('Enter your name: ')
+#
+#     return name == user_name.lower()
+#
+#
+# if __name__ == '__main__':
+#     print(name_check())
+
+# Program_4:
+
+def is_name_valid(name_from_storage: str, name_from_input: str) -> bool:
+    return name_from_storage == name_from_input
+
+
+def name_check() -> None:
+    name = 'mikhail'
+    user_name = input('Enter your name: ')
+
+    is_equal = is_name_valid(name, user_name)
+
+    message_about_state = '' if is_equal else ' not'
+    print(f'Name {name.title()} is{message_about_state} equal to {user_name.title()}')
+
+
+if __name__ == '__main__':
+    name_check()
