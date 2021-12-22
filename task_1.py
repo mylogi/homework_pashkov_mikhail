80d9fb3ec71be4c575926056449bf5330f46be85
mylogi <mp.gplay@gmail.com>
2021-12-22 16:29:20 +0200

    🔥 Added task_1 in lesson_07.

--- task_1.py
+++ task_1.py
@@ -1,2 +1,30 @@
-# Use Ctrl + Alt + L AutoFormatting.
-print('hello world')
+"""The Guessing Game.
+
+Write a program that generates a random number between 1 and 10 and lets the user guess what number was generated.
+The result should be sent back to the user via a print statement.
+"""
+
+# Program_game:
+import random
+
+
+def game_data(random_number: int, user_number: int) -> bool:
+    return random_number == user_number
+
+
+def generate():
+    print('Welcome in the Big Ten!')
+    computer_number = random.randint(1, 10)
+    user_input = int(input("Enter a number from 1 to 10: "))
+    if user_input == 0 or user_input > 10:
+        print('Incorrect input!')
+    else:
+        is_equal = game_data(computer_number, user_input)
+
+        result = f'{user_input} - it`s right! You win!' if is_equal else 'You lost!'
+
+        print(result)
+
+
+if __name__ == '__main__':
+    generate()
