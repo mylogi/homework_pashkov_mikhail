b1f24406f3f7243de36db02e474730057bfe32e7
mylogi <mp.gplay@gmail.com>
2021-12-22 16:55:12 +0200

    🔥 Added task_2 in lesson_07.

--- task_2.py
+++ task_2.py
@@ -1,3 +1,24 @@
-print('Play with git :)')
+""" The birthday greeting program.
 
-print('Play with git in merge lesson')
+Write a program that takes your name as input, and then your age as input and greets you with the following:
+
+“Hello <name>, on your next birthday you’ll be <age+1> years”
+"""
+
+
+# Program:
+
+def calculate_u_bd():
+    user_name = input("Enter your name: ")
+    user_age = input("Enter your age: ")
+
+    if user_age.isdigit() and user_name.isalpha():
+        user_age_add = int(user_age)
+        user_age_add += 1
+        print(f'Hello {user_name.title()}, on your next birthday you’ll be {user_age_add} years')
+    else:
+        print('Try once more')
+
+
+if __name__ == '__main__':
+    calculate_u_bd()
