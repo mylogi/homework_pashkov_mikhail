4d2ea05de3e4214767b6352627fb0a3a0e5f264b
mylogi <mp.gplay@gmail.com>
2021-12-23 17:27:22 +0200

    🌮 Prepared file task_2.

---
+++ task_2.py
@@ -0,0 +1,7 @@
+""" Task 2
+Write a function that takes in two numbers from the user via input(), call the numbers a and b, and then returns the
+value of squared a divided by b, construct a try-except block which raises an exception if the two values given by the
+input function were not numbers, and if value b was zero (cannot divide by zero).
+"""
+
+# Functions:
