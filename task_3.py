fdefba5fb49dffd1f52560da2045acef128dc7f0
mylogi <mp.gplay@gmail.com>
2021-12-27 11:45:59 +0200

    🌮 Prepared the file for the task.

--- task_3.py
+++ task_3.py
@@ -9,21 +9,3 @@ For example, the program obtained the word ‘hello’, so it should print 5 ran
 Tips: Use random module to get random char from string)
 """
 # Program:
-# import random
-#
-# user_string = input('Enter your word: ')
-# user_string_length = len(user_string)
-#
-#
-# def random_string_func():
-#     if user_string.isalpha():
-#         print(user_string[0:random.randint(0, user_string_length)])
-#
-#
-# if __name__ == '__main__':
-#     random_string_func()
-
-word = input('Enter the word: ')
-length = len(word)
-last_index = length - 1
-print(last_index)
