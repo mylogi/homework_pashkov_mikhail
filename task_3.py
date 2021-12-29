0c185955a29f7cdcf4282461e91443b23763083f
mylogi <mp.gplay@gmail.com>
2021-12-29 16:02:44 +0200

    Added task_3 in the lesson_07.

--- task_3.py
+++ task_3.py
@@ -8,4 +8,31 @@ For example, the program obtained the word ‘hello’, so it should print 5 ran
 
 Tips: Use random module to get random char from string)
 """
+
 # Program:
+import random
+
+
+def get_string(input_string: str) -> list[str]:
+    result = []
+    required_amount_of_words = len(input_string)
+    while len(result) < required_amount_of_words:
+        input_word_as_list = list(input_string)
+        random.shuffle(input_word_as_list)
+        new_word = ''.join(input_word_as_list)
+
+        if new_word not in result:
+            result.append(new_word)
+
+    return result
+
+
+def main():
+    input_string = input('Enter your word: ')
+    result = get_string(input_string=input_string)
+
+    print('\n', result[0], result[1], result[2], result[3], result[4], sep='\n')
+
+
+if __name__ == '__main__':
+    main()
