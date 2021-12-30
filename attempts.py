fbc866184a03c4b1e36c58199f6ccb021c3bee60
mylogi <mp.gplay@gmail.com>
2021-12-30 19:05:47 +0200

    ➕ Added ile with samples.

---
+++ attempts.py
@@ -0,0 +1,51 @@
+# x = 10
+#
+#
+# def my_func(a, b):
+#     print(x)
+#     print(z)
+#
+#
+# my_func(1, 2)
+
+
+# def my_func(a, b):
+#     i = 2
+#     print(x)
+#
+#
+# if __name__ == '__main__':
+#     x = 10
+#     my_func(1, 2)
+#     print(i)
+
+# def my_func(a, b):
+#     x = 5
+#     print(x)
+#
+#
+# if __name__ == '__main__':
+#     x = 10
+#     my_func(1, 2)
+#     print(x)
+
+
+# def my_func(a, b):
+#     global c
+#     b, a = a, b
+#     d = 'Mike'
+#     print(a, b, c, d)
+#
+#
+# a, b, c, d = 1, 2, 'c is global', 4
+# my_func(1, 2)
+# print(a, b, c, d)
+
+# def factorial(n):
+#     pr = 1
+#     for i in range(2, n + 1):
+#         pr = pr * i
+#     print(pr)
+#
+#
+# factorial(5)
