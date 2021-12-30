26ee44924b6a2f3b8a1bbe508476c65f33602ab0
mylogi <mp.gplay@gmail.com>
2021-12-30 12:24:16 +0200

    📤 First attempt in task_4.

--- task_4.py
+++ task_4.py
@@ -2,8 +2,30 @@
 
 Write a program that asks the answer for a mathematical expression, checks whether the user is right or wrong, and then
 responds with a message accordingly.
+
 """
 
 # Program:
+import random
 
-# Variables:
+x = random.randint(0, 1000)
+y = random.randint(0, 1000)
+
+
+def def_of_dictionary(first_variable: int, second_variable: int) -> dict:
+    dictionary_of_expression = {
+        f'{x} + {y} =': x + y,
+        f'{x} * {y} =': x * y,
+        f'{x} / {y} =': x / y
+    }
+    return dictionary_of_expression
+
+
+def print_of_key():
+    print_dictionary = def_of_dictionary(x, y)
+    i = 1
+    for keys in print_dictionary.keys():
+        print(keys)
+
+
+print_of_key()
