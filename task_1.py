aed883ad32181dea8e275efcfeeff7efc987fef2
mylogi <mp.gplay@gmail.com>
2021-12-31 13:32:46 +0200

    ➕ Added task_1 to the lesson_09.

--- task_1.py
+++ task_1.py
@@ -1,2 +1,29 @@
-# Use Ctrl + Alt + L AutoFormatting.
-print('hello world')
+"""The greatest number
+
+Write a Python program to get the largest number from a list of random numbers with the length of 10
+
+Constraints: use only while loop and random module to generate numbers
+
+"""
+
+# Program:
+import random
+
+
+def append_my_list() -> int:
+    my_list = []
+    i = 10
+    while i != 0:
+        my_list.append(random.randint(0, 100))
+        i -= 1
+    print('Our list of values:', *my_list)
+    my_list.sort()
+    return my_list[9]
+
+
+def main():
+    print('Our the largest numer:', append_my_list())
+
+
+if __name__ == '__main__':
+    main()
