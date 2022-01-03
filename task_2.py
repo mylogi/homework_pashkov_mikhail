ca25df393de8eb5e5fab513ab3f8362544ce685b
mylogi <mp.gplay@gmail.com>
2022-01-03 12:34:03 +0200

    Prepared the file for the task.

--- task_2.py
+++ task_2.py
@@ -1,1 +1,10 @@
+"""Exclusive common numbers.
 
+Generate 2 lists with the length of 10 with random integers from 1 to 10, and make a third list containing the common
+integers between the 2 initial lists without any duplicates.
+
+Constraints: use only while loop and random module to generate numbers
+
+"""
+
+# Function:
