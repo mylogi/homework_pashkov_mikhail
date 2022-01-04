9ce199b8c5095380edfe3484427f031895ae6a60
mylogi <mp.gplay@gmail.com>
2022-01-04 14:39:56 +0200

    ✨ Added the task_2 to lesson_09.

--- task_2.py
+++ task_2.py
@@ -8,3 +8,44 @@ Constraints: use only while loop and random module to generate numbers
 """
 
 # Function:
+import random
+
+
+def first_list():
+    list_number_one = []
+    i_1 = 0
+    while i_1 < 10:
+        list_number_one.append(random.randint(0, 99))
+        i_1 += 1
+    print(list_number_one)
+    return list_number_one
+
+
+def second_list():
+    list_number_two = []
+    i_2 = 0
+    while i_2 < 10:
+        list_number_two.append(random.randint(0, 99))
+        i_2 += 1
+    print(list_number_two)
+    return list_number_two
+
+
+def compare_lists():
+    list_number_01 = first_list()
+    list_number_02 = second_list()
+    list_sum = list_number_01 + list_number_02
+    result_list = [x for x in list_sum if x in list_number_01 and x in list_number_02]
+    return list(set(result_list))
+
+
+def main():
+    result_list = compare_lists()
+    if not result_list:
+        print('There is no data.')
+    else:
+        print(f'List after comparison without duplicates: {result_list}')
+
+
+if __name__ == '__main__':
+    main()
