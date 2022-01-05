0f43be73e0f2809d1766b1f03dd5efbf981899f8
mylogi <mp.gplay@gmail.com>
2022-01-05 20:30:57 +0200

    Prepared files for assignments.

---
+++ task_1.py
@@ -0,0 +1,7 @@
+"""
+Make a program that has some sentence (a string) on input and returns a dict containing all unique words as keys and
+the number of occurrences as values.
+
+"""
+
+# Program:
---
+++ task_2.py
@@ -0,0 +1,22 @@
+"""
+
+Input data:
+
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+Create a function which takes as input two dicts with structure mentioned above, then computes and returns the total
+price of stock.
+
+"""
+
+# Function:
