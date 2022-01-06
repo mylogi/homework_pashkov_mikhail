d7027a84fabc2bb40b732422cc3422f5599b9907
mylogi <mp.gplay@gmail.com>
2022-01-06 18:09:15 +0200

    ✨ Added task_2 to the lesson_10.

--- task_2.py
+++ task_2.py
@@ -20,3 +20,35 @@ price of stock.
 """
 
 # Function:
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+
+
+def work_with_dictionary():
+    new_dict = {}
+    for key_stock, value_stock in stock.items():
+        for key_prices, value_prices in prices.items():
+            if key_stock == key_prices:
+                full_price = int(value_stock * value_prices)
+                new_dict[key_stock] = full_price
+    return new_dict
+
+
+def main():
+    for key, value in work_with_dictionary().items():
+        key_upper = key.title()
+        print(f'{key_upper} price: {value}')
+
+
+if __name__ == '__main__':
+    main()
