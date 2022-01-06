40323b4f6232718bf0bc503e5c036c3354b9e282
mylogi <mp.gplay@gmail.com>
2022-01-06 16:57:54 +0200

    ✨ Added task_1 to the lesson_10.

--- task_1.py
+++ task_1.py
@@ -5,3 +5,21 @@ the number of occurrences as values.
 """
 
 # Program:
+import re
+
+
+def main():
+    my_str = input('Enter your sentence: ')
+    my_str_split_1 = re.split(r'[ ,:;\[\]]+', my_str)
+    new_dictionary = {}
+    print(my_str_split_1)
+    for word in my_str_split_1:
+        if word in new_dictionary:
+            new_dictionary[word] += 1
+        else:
+            new_dictionary[word] = 1
+    print(new_dictionary)
+
+
+if __name__ == '__main__':
+    main()
