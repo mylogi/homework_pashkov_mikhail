4263892415ab2bd415fea1852e2ddf7cb58ab201
mylogi <mp.gplay@gmail.com>
2022-01-06 18:40:21 +0200

    Added file my_attempts.

---
+++ my_attempts.py
@@ -0,0 +1,38 @@
+# # class MyObjects:
+# #
+# #     def __init__(self, name: str, size: str, age: int):
+# #         self.name = name
+# #         self.size = size
+# #         self.age = age
+# #
+# #     def break_object(self):
+# #         self.age -= 2
+# #
+# #     @staticmethod
+# #     def take_object():
+# #         return "Object taken"
+# #
+# #     @property
+# #     def give_information(self):
+# #         return f'{self.name}, {self.size}, {self.age}'
+# #
+# #
+# # def main():
+# #     pc = MyObjects('PC', 'Big', 2)
+# #     print(pc.name)
+# #     print(pc.size)
+# #     print(pc.age)
+# #     pc.break_object()
+# #     print(pc.age)
+# #     print(pc.give_information)
+# #
+# #
+# # if __name__ == '__main__':
+# #     main()
+#
+#
+# country = {'name': 'Sweden', 'capital': 'Stockholm', 'largest_cities': ['Stockholm', 'Gothenburg', 'Malmo'],
+#            'population': 10000000}
+# print(country)
+# country.update({'language': 'Swedish'})
+# print(country)
