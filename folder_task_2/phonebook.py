be2da4e65d95a723ca6f195ba9d1503c847f6f83
mylogi <mp.gplay@gmail.com>
2022-01-10 13:00:25 +0200

    ✨ Added new to the task_2.

---
+++ folder_task_2/phonebook.py
@@ -0,0 +1,9 @@
+# path_to_this_json = path_to_parent.joinpath('app_data.json')
+#
+# data_as_string = json.dumps(data_information(), indent=2)
+#
+# path_to_this_json.write_text(data_as_string)
+#
+# readed_data_as_string = path_to_this_json.read_text()
+#
+# readed_data = json.loads(readed_data_as_string)
