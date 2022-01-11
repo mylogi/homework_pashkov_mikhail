e692b8c19b4029e21c23ee66e3cccf2c51bc6fb6
mylogi <mp.gplay@gmail.com>
2022-01-11 19:42:02 +0200

    ✨ Added some change.

--- task_2.py
+++ task_2.py
@@ -1,3 +1,27 @@
-print('Play with git :)')
+"""Doggy age
 
-print('Play with git in merge lesson')
+Create a class Dog with class attribute `age_factor` equals to 7. Make __init__() which takes values for a dog’s age.
+Then create a method `human_age` which returns the dog’s age in human equivalent.
+
+"""
+
+
+# Sample:
+
+class Dog:
+    age_factor = 7
+
+    def __init__(self, age):
+        self.age = age
+
+    def human_age(self):
+        print(self.age * Dog.age_factor)
+
+
+def main():
+    dog_1 = Dog(7)
+    dog_1.human_age()
+
+
+if __name__ == '__main__':
+    main()
