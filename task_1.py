16711e775fefebfed8b73e5644ffee85bd873aea
mylogi <mp.gplay@gmail.com>
2022-01-11 15:09:13 +0200

    ✨ Added task_1 to the lesson_12.

--- task_1.py
+++ task_1.py
@@ -1,2 +1,30 @@
-# Use Ctrl + Alt + L AutoFormatting.
-print('hello world')
+"""A Person class
+
+Make a class called Person. Make the __init__() method take firstname, lastname, and age as parameters and add them as
+attributes. Make another method called talk() which makes prints a greeting from the person containing, for example
+like this: “Hello, my name is Carl Johnson and I’m 26 years old”.
+
+"""
+
+
+def talk(firstname, lastname, age):
+    print(f'Hello, my name is {firstname} {lastname} and I’m {age} years old')
+
+
+class Person:
+    firstname = 'Carl'
+    lastname = 'Johnson'
+    age = 26
+
+    def __init__(self, firstname: str, lastname: str, age: int):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+
+
+def main():
+    talk(Person.firstname, Person.lastname, Person.age)
+
+
+if __name__ == '__main__':
+    main()
