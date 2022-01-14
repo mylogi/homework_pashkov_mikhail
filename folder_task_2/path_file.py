dde91fe0e8a78c4407167f055454fbd269a7e52c
mylogi <mp.gplay@gmail.com>
2022-01-14 12:41:31 +0200

    New attempts.

--- folder_task_2/path_file.py
+++ folder_task_2/path_file.py
@@ -4,7 +4,11 @@ path_to_this_file = pathlib.Path(__file__)
 
 path_to_parent = path_to_this_file.parent
 
+path_to_json_file = path_to_parent.joinpath('phone_book.json')
+
 if __name__ == '__main__':
     print(path_to_this_file)
 
     print(path_to_parent)
+
+    print(path_to_json_file)
--- folder_task_2/phonebook.py
+++ folder_task_2/phonebook.py
@@ -1,9 +1,71 @@
-# path_to_this_json = path_to_parent.joinpath('app_data.json')
-#
-# data_as_string = json.dumps(data_information(), indent=2)
-#
-# path_to_this_json.write_text(data_as_string)
-#
-# readed_data_as_string = path_to_this_json.read_text()
-#
-# readed_data = json.loads(readed_data_as_string)
+import json
+
+from path_file import path_to_json_file
+
+
+def first_task_after_turn():
+    phonebook_dict_json_loads = path_to_json_file.read_text()
+
+    return json.loads(phonebook_dict_json_loads)
+
+
+def first_question():
+    first_input = input(
+        'Hello! What does number interest you?\nDont know number - please pass s\nOr pass number:').lower()
+    # if first_input == 's':
+    #     return first_input
+    # elif first_input.isdigit() and len(first_input) == 10:
+    #     return first_input
+    if first_input != 's' or (first_input.isdigit() and len(first_input) == 10):
+        first_question()
+    else:
+        return first_input
+
+
+def search(search_query, readed_data):
+    if search_query == 's':
+        search_input = input('\nPlease choice:\nFirst name search - F'
+                             '\nLast name search - L'
+                             '\nCity search - C'
+                             '\nState search - S'
+                             '\nFull name search - FL'
+                             '\nTelephone search - T'
+                             '\nDo choice: ').lower()
+        if search_input in ['f', 'l', 'c', 's', 'fl', 't']:
+            return search_input
+    else:
+        for key, value in readed_data.items():
+            if key == search_query:
+                print(f'{key}: {value}')
+                return False
+
+
+def search_first_name(search_input, readed_data):
+    if not search_input:
+        return False
+    elif search_input == 'f':
+        first_name_input = input('Enter the first name: ').lower()
+        for key, value in readed_data.items():
+            if value['first_name'].lower() == first_name_input:
+                print(f'{key}: {value}')
+                main()
+            else:
+                return True
+
+
+def last_task_after_turn(readed_data):
+    phonebook_dict_json_dumps = json.dumps(readed_data, indent=2)
+
+    path_to_json_file.write_text(phonebook_dict_json_dumps)
+
+
+def main():
+    readed_data = first_task_after_turn()
+    # last_task_after_turn(readed_data)
+    search_query = first_question()
+    search_input = search(search_query, readed_data)
+    search_first_name(search_input, readed_data)
+
+
+if __name__ == '__main__':
+    main()
