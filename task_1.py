ead6614e56d28265ac7a19c6f3aa10b385f7a3e4
mylogi <mp.gplay@gmail.com>
2022-01-28 13:49:16 +0200

    Prepared the file to the task_1.

---
+++ task_1.py
@@ -0,0 +1,8 @@
+"""School
+
+Make a class structure in python representing people at school. Make a base class called Person, a class called Student,
+and another one called Teacher. Try to find as many methods and attributes as you can which belong to different classes,
+and keep in mind which are common and which are not. For example, the name should be a Person attribute, while salary
+should only be available to the teacher.
+
+"""
