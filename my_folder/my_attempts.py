309654b061161e9a9f066f43e2fbd0a0eabbd91e
mylogi <mp.gplay@gmail.com>
2022-01-31 21:14:13 +0200

    ✨ Added a few examples to my_attempts.py and prepared task_1/task_2.

---
+++ my_folder/my_attempts.py
@@ -0,0 +1,28 @@
+# BIG-O
+# Example 1:
+
+# def find_mind(alist):
+#     overallim = alist[0]
+#     for i in alist:
+#         issmallest = True
+#         for j in alist:
+#             if i > j:
+#                 issmallest = False
+#         if issmallest:
+#             overallim = i
+#     return overallim
+#
+#
+# print(find_mind([5, 4, 3, 2, 1, 0]))
+# print(find_mind([0, 1, 2, 3, 4, 5]))
+# print(find_mind([7, 8, 10, 2, 3, 10, 11]))
+#
+#
+# def fact(n):
+#     product = 1
+#     for i in range(n):
+#         product = product * (i + 1)
+#     return product
+#
+#
+# print(fact(5))
