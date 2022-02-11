e6f683d94e1d2f70a6a37849854749de13a37d2f
mylogi <mp.gplay@gmail.com>
2022-02-11 17:46:43 +0200

    ➡ Moved the task_2.py to my_folder.

---
+++ my_folder/task_2.py
@@ -0,0 +1,8 @@
+"""
+
+Modify the `build_parse_tree` and `evaluate functions to handle boolean statements (and, or, and not). Remember that
+“not” is a unary operator, so this will somewhat complicate your code.
+
+"""
+
+# Solution:
