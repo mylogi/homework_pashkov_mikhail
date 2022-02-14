4dfffbeb85fdfbd78274654e68cd0f7bde09e986
mylogi <mp.gplay@gmail.com>
2022-02-14 12:47:56 +0200

    ✨ Added new version of task_1.

--- task_1.py
+++ task_1.py
@@ -6,30 +6,65 @@ describe under what circumstances it might be appropriate.
 
 """
 
+
 # Solution:
-import random
+# import random
+#
+#
+# def bubble_sort_up(array):
+#     for pass_num in range(len(array) - 1, 0, -1):
+#         for i in range(pass_num):
+#             if array[i] > array[i + 1]:
+#                 temp = array[i]
+#                 array[i] = array[i + 1]
+#                 array[i + 1] = temp
+#
+#
+# def bubble_sort_down(array):
+#     for pass_num in range(len(array) - 1):
+#         for i in range(len(array) - 1, pass_num, -1):
+#             if array[i] > array[i - 1]:
+#                 temp = array[i]
+#                 array[i] = array[i - 1]
+#                 array[i - 1] = temp
+#
+#
+# list_01 = [random.randint(1, 100) for _ in range(26)]
+# bubble_sort_up(list_01)
+# print(list_01, '\n')
+# bubble_sort_down(list_01)
+# print(list_01, '\n')
 
 
-def bubble_sort_up(array):
-    for pass_num in range(len(array) - 1, 0, -1):
-        for i in range(pass_num):
-            if array[i] > array[i + 1]:
-                temp = array[i]
-                array[i] = array[i + 1]
-                array[i + 1] = temp
+def bubble_sort_2_ways(list_to_sort: list[int]) -> list[int]:
+    swapped = True
+    start_index = 0
+    end_index = len(list_to_sort) - 1
+
+    while swapped:
+        swapped = False
+
+        for i in range(start_index, end_index):
+            if list_to_sort[i] > list_to_sort[i + 1]:
+                list_to_sort[i], list_to_sort[i + 1] = list_to_sort[i + 1], list_to_sort[i]
+                swapped = True
+
+        if not swapped:
+            break
+
+        swapped = False
+        end_index -= 1
+
+        for i in range(end_index - 1, start_index - 1, -1):
+            if list_to_sort[i] > list_to_sort[i + 1]:
+                list_to_sort[i], list_to_sort[i + 1] = list_to_sort[i + 1], list_to_sort[i]
+                swapped = True
+
+        start_index += 1
+
+    return list_to_sort
 
 
-def bubble_sort_down(array):
-    for pass_num in range(len(array) - 1):
-        for i in range(len(array) - 1, pass_num, -1):
-            if array[i] > array[i - 1]:
-                temp = array[i]
-                array[i] = array[i - 1]
-                array[i - 1] = temp
-
-
-list_01 = [random.randint(1, 100) for _ in range(26)]
-bubble_sort_up(list_01)
-print(list_01, '\n')
-bubble_sort_down(list_01)
-print(list_01, '\n')
+input_list = [5, 7, 6, 8, 4, 5, 0, 4, 7, 9, 2, 1]
+bubble_sort_2_ways(input_list)
+print(input_list)
