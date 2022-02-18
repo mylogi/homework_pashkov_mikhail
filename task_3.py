90d46a28db54d5123d1820652538be07b79a5ca7
mylogi <mp.gplay@gmail.com>
2022-02-18 14:12:01 +0200

    ✨ Added task_3 to lesson_29.

--- task_3.py
+++ task_3.py
@@ -6,62 +6,67 @@ analysis using different list sizes for the partition limit.
 
 """
 
+
 # Solution:
 
 
-# def quick_sort(array):
-#     quick_sort_helper(array, 0, len(array) - 1)
-#
-#
-# def quick_sort_helper(array, first, last):
-#     if first < last:
-#         split_point = partition(array, first, last)
-#
-#         quick_sort_helper(array, first, split_point - 1)
-#         quick_sort_helper(array, split_point + 1, last)
-#
-#
-# def partition(array, first, last):
-#     pivot_value = array[first]
-#
-#     left_mark = first + 1
-#     right_mark = last
-#
-#     done = False
-#     while not done:
-#
-#         while left_mark <= right_mark and array[left_mark] <= pivot_value:
-#             left_mark = left_mark + 1
-#
-#         while array[right_mark] >= pivot_value and right_mark >= left_mark:
-#             right_mark = right_mark - 1
-#
-#         if right_mark < left_mark:
-#             done = True
-#         else:
-#             temp = array[left_mark]
-#             array[left_mark] = array[right_mark]
-#             array[right_mark] = temp
-#
-#     temp = array[first]
-#     array[first] = array[right_mark]
-#     array[right_mark] = temp
-#
-#     return right_mark
-#
-#
-# def selection_sort(array):
-#     for fill_slot in range(len(array) - 1, 0, -1):
-#         position_max = 0
-#         for location in range(1, fill_slot + 1):
-#             if array[location] > array[position_max]:
-#                 position_max = location
-#
-#         temp = array[fill_slot]
-#         array[fill_slot] = array[position_max]
-#         array[position_max] = temp
-#
-#
-# list_01 = [random.randint(1, 100) for _ in range(3)]
-# selection_sort(list_01)
-# print(list_01, '\n')
+def insertion_sort(nums):
+    for i in range(1, len(nums)):
+        item_to_insert = nums[i]
+        j = i - 1
+        while j >= 0 and nums[j] > item_to_insert:
+            nums[j + 1] = nums[j]
+            j -= 1
+        nums[j + 1] = item_to_insert
+
+
+def quick_sort(array):
+    if len(array) < 10:
+        insertion_sort(array)
+
+    else:
+        quick_sort_helper(array, 0, len(array) - 1)
+
+
+def quick_sort_helper(array, first, last):
+    if first < last:
+        split_point = partition(array, first, last)
+
+        quick_sort_helper(array, first, split_point - 1)
+        quick_sort_helper(array, split_point + 1, last)
+
+
+def partition(array, first, last):
+    pivot_value = array[first]
+
+    left_mark = first + 1
+    right_mark = last
+
+    done = False
+    while not done:
+
+        while left_mark <= right_mark and array[left_mark] <= pivot_value:
+            left_mark = left_mark + 1
+
+        while array[right_mark] >= pivot_value and right_mark >= left_mark:
+            right_mark = right_mark - 1
+
+        if right_mark < left_mark:
+            done = True
+        else:
+            temp = array[left_mark]
+            array[left_mark] = array[right_mark]
+            array[right_mark] = temp
+
+    temp = array[first]
+    array[first] = array[right_mark]
+    array[right_mark] = temp
+    return right_mark
+
+
+random_list_of_nums = [2, 33, 9, 36, 93, 3, 11, 14]
+random_list_of_nums_2 = [33, 11, 92, 93, 1, 13, 4, 8, 9, 10]
+quick_sort(random_list_of_nums)
+quick_sort(random_list_of_nums_2)
+print(random_list_of_nums)
+print(random_list_of_nums_2)
