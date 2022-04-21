1b93af4b292b970f16bf766f34fd1f759c95f5bc
mylogi <mp.gplay@gmail.com>
2022-04-21 15:31:27 +0300

    ✨ Prepared task_2.py file.

---
+++ task_2.py
@@ -0,0 +1,13 @@
+"""Task 2
+
+Implement 2 classes, the first one is the Boss and the second one is the Worker.
+
+Worker has a property 'boss', and its value must be an instance of Boss.
+
+You can reassign this value, but you should check whether the new value is Boss. Each Boss has a list of his own
+workers. You should implement a method that allows you to add workers to a Boss. You're not allowed to add instances of
+Boss class to workers list directly via access to attribute, use getters and setters instead!
+
+"""
+
+# Solution:
