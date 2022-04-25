8fd64b8a992f8d4b05a3d6ae16c947149d3b78be
mylogi <mp.gplay@gmail.com>
2022-04-25 18:09:01 +0300

    ✨ Reworked some functions.
    1) send_message and added authorization.

--- Joker_Chuck_Chat/joker_chat_server.py
+++ Joker_Chuck_Chat/joker_chat_server.py
@@ -6,11 +6,21 @@ import aiohttp
 
 list_of_connection = []
 
+users_data = {}
+
+
+class Client:
+
+    def __init__(self, connection):
+        self.connection = connection
+        self.user_name: str = ''
+        self.password: str = ''
+
 
 async def echo(connection: socket, loop: AbstractEventLoop) -> None:
     while data := await loop.sock_recv(connection, 1024):
         # await loop.sock_sendall(connection, data)
-        await send_message_to_everyone(data.decode())
+        await send_message(data.decode())
 
 
 async def listen_for_connection(server_socket: socket,
@@ -18,22 +28,48 @@ async def listen_for_connection(server_socket: socket,
     while True:
         connection, address = await loop.sock_accept(server_socket)
         connection.setblocking(False)
-        list_of_connection.append(connection)
+        client = Client(connection)
+        list_of_connection.append(client)
         print(f"Got a connection from {address}")
+        await authorization(client)
         asyncio.create_task(echo(connection, loop))
 
 
-async def send_message_to_everyone(message):
+# async def send_message_to_everyone(message):
+#     loop = asyncio.get_event_loop()
+#     global list_of_connection
+#     copy_list_of_connection = list_of_connection[:]
+#     for client in list_of_connection:
+#         try:
+#             await loop.sock_sendall(client.connection, message.encode())
+#         except Exception as e:
+#             print(e)
+#             copy_list_of_connection.remove(client)
+#     list_of_connection = copy_list_of_connection[:]
+
+
+async def send_message(message, client: Client = None):
     loop = asyncio.get_event_loop()
     global list_of_connection
     copy_list_of_connection = list_of_connection[:]
-    for connection in list_of_connection:
+    if client is None:
+        for client in list_of_connection:
+            try:
+                await loop.sock_sendall(client.connection, message.encode())
+            except Exception as e:
+                print(e)
+                copy_list_of_connection.remove(client)
+    else:
         try:
-            await loop.sock_sendall(connection, message.encode())
+            await loop.sock_sendall(client.connection, message.encode())
         except Exception as e:
             print(e)
-            copy_list_of_connection.remove(connection)
+            copy_list_of_connection.remove(client)
     list_of_connection = copy_list_of_connection[:]
+
+
+async def authorization(client: Client):
+    await send_message('test', client)
 
 
 async def joke_by_chuck():
@@ -43,7 +79,7 @@ async def joke_by_chuck():
             async with session.get(url) as resp:
                 raw = await resp.json()
                 joke_value = raw['value']
-                await send_message_to_everyone(joke_value)
+                await send_message(joke_value)
                 await asyncio.sleep(60)
                 # print(resp.status)
                 # print(await resp.text())
