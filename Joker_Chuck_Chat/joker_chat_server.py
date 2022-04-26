6780bf99f82484eb854509efc9dd03efb755e804
mylogi <mp.gplay@gmail.com>
2022-04-26 12:32:23 +0300

    ✨ Rework authorization.
    1) don't test everything cases

--- Joker_Chuck_Chat/joker_chat_server.py
+++ Joker_Chuck_Chat/joker_chat_server.py
@@ -82,36 +82,61 @@ async def send_message(message, client: Client = None):
     # list_of_auth_users = copy_list_of_connection[:]
 
 
+# async def authorization_first_variant(client: Client):
+#     loop = asyncio.get_event_loop()
+#     await send_message('Please enter your user name: ', client)
+#     # while data := await loop.sock_recv(client.connection, 1024):
+#     if client.user_name == '':
+#         client.user_name = await recive_data(client, loop)
+#         if client.user_name in users_data.keys():
+#             await send_message('User name is already exists ', client)
+#             client.user_name = ''
+#             client.password = ''
+#             await authorization(client)
+#         await send_message('Please enter your password: ', client)
+#     if client.password == '' and client.user_name != '':
+#         client.password = await recive_data(client, loop)
+#     if client.user_name in users_data.keys():
+#         if client.password == users_data[client.user_name].password:
+#             await send_message(f'Welcome {client.user_name}!', client)
+#             client.authorized = True
+#         else:
+#             await send_message('Password is incorrect\n', client)
+#             client.user_name = ''
+#             client.password = ''
+#             await authorization(client)
+#     else:
+#         await send_message(f'Welcome {client.user_name}!', client)
+#         users_data[client.user_name] = client
+#         print(f'{client.user_name} - {client.password}')
+#         chat_task = asyncio.create_task(chat(client, asyncio.get_event_loop()))
+#         # await chat_task
+#         # list_of_auth_users.append(client)
+
+
 async def authorization(client: Client):
     loop = asyncio.get_event_loop()
-    await send_message('Please enter your user name: ', client)
-    # while data := await loop.sock_recv(client.connection, 1024):
-    if client.user_name == '':
+    n = 3
+    while n > 0:
+        await send_message('Please enter your user name: ', client)
         client.user_name = await recive_data(client, loop)
-        if client.user_name in users_data.keys():
-            await send_message('User name is already exists ', client)
-            client.user_name = ''
-            client.password = ''
-            await authorization(client)
         await send_message('Please enter your password: ', client)
-    if client.password == '' and client.user_name != '':
         client.password = await recive_data(client, loop)
-    if client.user_name in users_data.keys():
-        if client.password == users_data[client.user_name].password:
-            await send_message(f'Welcome {client.user_name}!', client)
-            client.authorized = True
+        if client.user_name in users_data.keys() and client.password == users_data[client.user_name].password:
+            await send_message(f'Welcome {client.user_name}', client)
+            break
+        elif client.user_name in users_data.keys() and client.password != users_data[client.user_name].password:
+            n -= 1
+            if n == 0:
+                await send_message('Incorrect password. Try another time!', client)
+            else:
+                await send_message(f'Incorrect password. {n} tries left!', client)
         else:
-            await send_message('Password is incorrect\n', client)
-            client.user_name = ''
-            client.password = ''
-            await authorization(client)
-    else:
-        await send_message(f'Welcome {client.user_name}!', client)
-        users_data[client.user_name] = client
-        print(f'{client.user_name} - {client.password}')
-        chat_task = asyncio.create_task(chat(client, asyncio.get_event_loop()))
-        # await chat_task
-        # list_of_auth_users.append(client)
+            await send_message(f'Welcome {client.user_name}!', client)
+            users_data[client.user_name] = client
+            print(f'User name: {client.user_name}. Password: {client.password}')
+            chat_task = asyncio.create_task(chat(client, asyncio.get_event_loop()))
+            break
 
 
 async def joke_by_chuck():
