23050e220582f1a10b56ea2404290b75f6c39767
mylogi <mp.gplay@gmail.com>
2022-05-02 17:58:06 +0300

    ✨ Added task_1.py (SQLite command in this file).
    1) and added new task1_database.db in .gitignore

--- task_1.py
+++ task_1.py
@@ -17,54 +17,47 @@ con = sqlite3.connect('task1_database.db')
 
 cursorObj = con.cursor()
 
-# def sql_table(con_example):
-#     cursorObj.execute(
-#         "CREATE TABLE employees(id integer PRIMARY KEY, name text, salary real, department text, position text, hireDate text)"
-#     )
-#
-#     con_example.commit()
+
+def sql_table(con_example):
+    cursorObj.execute(
+        "CREATE TABLE table1_try(id integer PRIMARY KEY, name text, salary real, department text, position text, hireDate text)"
+    )
+
+    con_example.commit()
 
 
-# def add_column(con_example):
-#     cursorObj.execute("ALTER TABLE employees ADD new_column_name text")
-#
-#     con_example.commit()
+def rename_db(con_example):
+    cursorObj.execute("ALTER TABLE table1_try RENAME TO employees")
+
+    con_example.commit()
 
 
-# def rename_db(con_example):
-#     cursorObj.execute("ALTER TABLE table1_try RENAME TO employees")
-#
-#     con_example.commit()
+def sql_insert(con_example, entities_tuple):
+    cursorObj.execute(
+        'INSERT INTO employees(id, name, salary, department, position, hireDate) VALUES(?, ?, ?, ?, ?, ?)',
+        entities_tuple)
 
-# def delete_column(con_example):
-# cursorObj.execute("ALTER TABLE employees RENAME TO _employees_old")
-# cursorObj.execute(
-#     "CREATE TABLE employees(id integer PRIMARY KEY, name text, salary real, department text, position text, hireDate text)")
+    con_example.commit()
 
-# list_of_insert =
-# cursorObj.execute(
-#     "INSERT INTO employees(id, name, salary, department, position, hireDate) VALUES(?, ?, ?, ?, ?, ?)", cursorObj.execute("SELECT * FROM _employees_old"))
-# con_example.commit()
 
-# def sql_insert(con_example, entities_tuple):
-#     cursorObj.execute(
-#         'INSERT INTO employees(id, name, salary, department, position, hireDate) VALUES(?, ?, ?, ?, ?, ?)',
-#         entities_tuple)
-#
-#     con_example.commit()
-#
-#
-# entities = (2, 'Andrew', 800, 'IT', 'Tech', '2018-02-06')
-#
-#
-# def sql_update(con_example):
-#     cursorObj.execute('UPDATE employees SET name = "Rogers" where id = 2')
-#
-#     con_example.commit()
+entities = (2, 'Andrew', 800, 'IT', 'Tech', '2018-02-06')
 
-# sql_table(con)
-# sql_insert(con, entities)
-# sql_update(con)
-# add_column(con)
-# rename_db(con)
-# delete_column(con)
+
+def sql_update(con_example):
+    cursorObj.execute('UPDATE employees SET name = "Rogers" where id = 2')
+
+    con_example.commit()
+
+
+def add_column(con_example):
+    cursorObj.execute("ALTER TABLE employees ADD new_column_name text")
+
+    con_example.commit()
+
+
+if __name__ == '__main__':
+    sql_table(con)
+    rename_db(con)
+    sql_insert(con, entities)
+    sql_update(con)
+    add_column(con)
